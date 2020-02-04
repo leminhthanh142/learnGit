a21cfc67d2880615101442c130ac2d04cbfc4447
ThuyLe12101995 <thuy.leminh31@gmail.com>
2020-02-04 22:00:54 +0700

    checkout develop

--- hello.js
+++ hello.js
@@ -2,6 +2,7 @@ const hello = () => {
     console.log('say hello world');
     console.log('demo conflict');
     console.log('in branch master');
+    console.log('demo checkout develop');
 };
 
 hello();
